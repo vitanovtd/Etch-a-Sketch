d896cfc253cdf075468301c041789593e2b37fcd
Tihomir Vitanov <vitanovtd@gmail.com>
2024-07-30 12:54:41 +0300

    added rezise grid option, styling added

--- script.js
+++ script.js
@@ -1,15 +1,29 @@
 const container = document.querySelector(".container");
+const resetButton = document.querySelector(".reset-button");
 
 function createGrid(size) {
-  container.innerHTML = ""; // Clear the container
+  container.innerHTML = "";
   for (let i = 0; i < size * size; i++) {
     const square = document.createElement("div");
     square.classList.add("grid-square");
     container.appendChild(square);
     square.addEventListener("mouseover", () => {
-      square.style.backgroundColor = "#333"; // Change to desired hover color
+      square.style.backgroundColor = "#333";
     });
   }
 }
+
+function promptForGridSize() {
+  let size = parseInt(
+    prompt("Enter the number of squares per side for the new grid")
+  );
+  if (isNaN(size) || size < 1 || size > 64) {
+    alert("Please enter a number between 1 and 64");
+  } else {
+    createGrid(size);
+  }
+}
+
+resetButton.addEventListener("click", promptForGridSize);
 
 createGrid(16);
